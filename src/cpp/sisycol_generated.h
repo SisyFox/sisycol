443edc2ed99d55ed11eaea50a783665529f10408
Viktor Schneider <info@vjs.io>
2019-06-23 17:01:47 +0200

    add PayPerPlay Setting

--- src/cpp/sisycol_generated.h
+++ src/cpp/sisycol_generated.h
@@ -816,6 +816,7 @@ enum SettingType {
   SettingType_SENSOR = 16,
   SettingType_MULTIPLAYER_ENABLED = 17,
   SettingType_VIDEO_VOLUME = 18,
+  SettingType_PAY_PER_PLAY_ENABLED = 19,
   SettingType_SF_CONTROL_INTERNAL = 63,
   SettingType_WORLD = 64,
   SettingType_LEVEL = 65,
@@ -834,7 +835,7 @@ enum SettingType {
   SettingType_MAX = SettingType_GAME_ENABLED
 };
 
-inline SettingType (&EnumValuesSettingType())[33] {
+inline SettingType (&EnumValuesSettingType())[34] {
   static SettingType values[] = {
     SettingType_GAME_LANGUAGE,
     SettingType_INTERFACE_LANGUAGE,
@@ -855,6 +856,7 @@ inline SettingType (&EnumValuesSettingType())[33] {
     SettingType_SENSOR,
     SettingType_MULTIPLAYER_ENABLED,
     SettingType_VIDEO_VOLUME,
+    SettingType_PAY_PER_PLAY_ENABLED,
     SettingType_SF_CONTROL_INTERNAL,
     SettingType_WORLD,
     SettingType_LEVEL,
@@ -894,7 +896,7 @@ inline const char **EnumNamesSettingType() {
     "SENSOR",
     "MULTIPLAYER_ENABLED",
     "VIDEO_VOLUME",
-    "",
+    "PAY_PER_PLAY_ENABLED",
     "",
     "",
     "",
